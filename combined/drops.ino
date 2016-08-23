eba5a189951e56b6ab992834f39cc571df16c520
Janardan Yri <janardan.yri@me.com>
2016-08-22 23:23:17 -0700

    Retune the gumball parameters, incl randomized strip drops

--- combined/drops.ino
+++ combined/drops.ino
@@ -1,22 +1,28 @@
 
+typedef struct {
+  uint8_t loc;
+  uint8_t strip;
+} Drop;
 
 // This sends drops down the sphere and then spiral, at random intervals :]
 // I'm pretty happy with this one :] -J
 void gumball_spiraling_randoms() {
   static bool initialized = false;
-  static const uint8_t DROP_COUNT = 8;
-  static const uint8_t DROP_PERCENT_CHANCE = 4; //originally 2
+  static const uint8_t DROP_COUNT = 12;
+  static const uint8_t DROP_PERCENT_CHANCE = 3; //originally 2
   static const uint8_t SPHERE_FADE = 220; //lower means shorter drops 230 is original for both
-  static const uint8_t SPIRAL_FADE = 200;
+  static const uint8_t SPIRAL_FADE = 180;
   static const uint8_t CYCLES_BETWEEN_DROPS = 10; //originally 10
 
-  static uint8_t drops[DROP_COUNT];
+  static Drop drops[DROP_COUNT];
+  static uint8_t last_three_strips[3];
   static uint8_t cycles_since_last_drop = 0;
 
   if (initialized == false) {
     initialized = true;
     for (int i = 0; i < DROP_COUNT; i++) {
-      drops[i] = random(SPIRAL_LED_COUNT);
+      drops[i].loc = random(SPIRAL_LED_COUNT);
+      drops[i].strip = random8(SPHERE_STRIP_COUNT);
     }
   }
 
@@ -37,14 +43,14 @@ void gumball_spiraling_randoms() {
   for (int d = 0; d < DROP_COUNT; d++) {
     uint8_t hue = d * (255 / DROP_COUNT);
 
-    if (drops[d] < SPHERE_LED_COUNT) {
+    if (drops[d].loc < SPHERE_LED_COUNT) {
       // Dropping down the sphere
-      sphere_leds[d][SPHERE_LED_COUNT - drops[d] - 1] = CHSV(hue, 255, 255);
-      drops[d] += 1;
-    } else if (drops[d] < SPHERE_LED_COUNT + SPIRAL_LED_COUNT) {
+      sphere_leds[drops[d].strip][SPHERE_LED_COUNT - drops[d].loc - 1] = CHSV(hue, 255, 255);
+      drops[d].loc += 1;
+    } else if (drops[d].loc < SPHERE_LED_COUNT + SPIRAL_LED_COUNT) {
       // Dropping down the strip
-      spiral_leds[SPIRAL_LED_COUNT - drops[d] + SPHERE_LED_COUNT - 1] = CHSV(hue, 255, 255);
-      drops[d] += 1;
+      spiral_leds[SPIRAL_LED_COUNT - drops[d].loc + SPHERE_LED_COUNT - 1] = CHSV(hue, 255, 255);
+      drops[d].loc += 1;
     } else {
       // This drop is done. Random chance of restarting.
       if (cycles_since_last_drop > CYCLES_BETWEEN_DROPS && random(100) < DROP_PERCENT_CHANCE) {
@@ -56,14 +62,28 @@ void gumball_spiraling_randoms() {
           Serial.print(d);
           Serial.print(" - ");
           for (int dd = 0; dd < DROP_COUNT; dd++) {
-            Serial.print(drops[dd]);
+            Serial.print(drops[dd].loc);
             Serial.print(",");
           }
           Serial.println("");
         }
         
-        drops[d] = 0;
-        spiral_leds[SPIRAL_LED_COUNT - drops[d] - 1] = CHSV(hue, 255, 255);
+        drops[d].loc = 0;
+
+        uint8_t random_strip = random8(SPHERE_STRIP_COUNT);
+
+        while (random_strip == last_three_strips[0]
+            || random_strip == last_three_strips[1]
+            || random_strip == last_three_strips[2]) {
+          random_strip = random8(SPHERE_STRIP_COUNT);
+        }
+
+        last_three_strips[2] = last_three_strips[1];
+        last_three_strips[1] = last_three_strips[0];
+        last_three_strips[0] = random_strip;
+        
+        drops[d].strip = random_strip;
+        spiral_leds[SPIRAL_LED_COUNT - drops[d].loc - 1] = CHSV(hue, 255, 255);
       }
     }
   }
